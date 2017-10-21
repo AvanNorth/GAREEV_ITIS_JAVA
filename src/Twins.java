e066a3faf9958e47f58992458f451d2de00aaea9
AvanNorth <kamilgareev39@gmail.com>
2017-10-21 16:54:52 +0300

    Дз к 21.10

--- src/Twins.java
+++ src/Twins.java
@@ -1,15 +1,16 @@
 public class Twins {
     public static void main(String[] args) {
-        int[] arr={1,2,4,4,5};
+        int[] arr={1,2,4,5,4};
         System.out.println(ArrTwin(arr));
     }
     static boolean ArrTwin(int[] arr){
         int n=arr.length;
         boolean bool=false;
-        for (int i=0;i<n-1;i++) {
-
-                if (arr[i]==arr[i+1]) {
-                    bool=true;
+        for (int i=0;i<n;i++) {
+            for (int j=i;j<n;j++){
+                if (arr[i]==arr[j]) {
+                    bool = true;
+                }
                 }
             }
         return bool;
